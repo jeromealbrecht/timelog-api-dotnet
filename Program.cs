b280e3c382c2adbdc1b93755857e297046fae717
December81 <megastudio@outlook.fr>
2025-05-08 22:44:56 +0200

    refactor : Suppression de la configuration d'écoute sur le port dans Program.cs, simplifiant ainsi la gestion des paramètres d'environnement.

--- Program.cs
+++ Program.cs
@@ -4,10 +4,6 @@ using TimeLog.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Config Render: écouter sur le bon port
-var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
-
 // Swagger + API Explorer
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
