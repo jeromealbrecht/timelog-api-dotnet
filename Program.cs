cf39c34ecdcee3fb40052682ea29753bc2c561a0
December81 <megastudio@outlook.fr>
2025-05-08 20:58:56 +0200

    feat : Création du fichier Program.cs pour la configuration de l'application, incluant la configuration de Kestrel, l'ajout de Swagger, la configuration de MongoDB et l'enregistrement des services nécessaires.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+using Microsoft.Extensions.Options;
+using TimeLog.Models;
+using TimeLog.Services;
+using Microsoft.AspNetCore.HttpsPolicy;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Supprime les URLs héritées
+builder.WebHost.UseSetting(WebHostDefaults.ServerUrlsKey, null);
+
+// Configure Kestrel
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    serverOptions.ListenLocalhost(5282); // HTTP
+    serverOptions.ListenLocalhost(7282, listenOptions => 
+    {
+        listenOptions.UseHttps();
+    }); // HTTPS
+});
+
+// Swagger + API Explorer
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Ajoute les contrôleurs
+builder.Services.AddControllers();
+
+// Configure MongoDB
+builder.Services.Configure<MongoDbSettings>(
+    builder.Configuration.GetSection("MongoDbSettings"));
+
+// Ajouter les services
+builder.Services.AddSingleton<UserService>();
+builder.Services.AddSingleton<TimeLogService>();
+builder.Services.AddTransient<KeyVaultService>();
+
+builder.Services.Configure<HttpsRedirectionOptions>(options =>
+{
+    options.HttpsPort = 5001; // Mets ici le port HTTPS que tu utilises
+});
+
+var app = builder.Build();
+
+// Dev only: Swagger UI
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
