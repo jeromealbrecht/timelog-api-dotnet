7368b42396ec07c3c8bb1023af0003f21520dc4c
December81 <megastudio@outlook.fr>
2025-05-08 22:25:20 +0200

    refactor : Suppression de la configuration Kestrel pour l'environnement de développement et la redirection HTTPS dans Program.cs, simplifiant ainsi la gestion des environnements et unifiant la configuration.

--- Program.cs
+++ Program.cs
@@ -4,12 +4,6 @@ using TimeLog.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Si on est sur Render (production), utilise le port d'environnement
-if (!builder.Environment.IsDevelopment())
-{
-    builder.WebHost.UseUrls("http://0.0.0.0:" + (Environment.GetEnvironmentVariable("PORT") ?? "8080"));
-}
-
 // Swagger + API Explorer
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -27,14 +21,6 @@ builder.Services.AddSingleton<TimeLogService>();
 builder.Services.AddTransient<KeyVaultService>();
 
 var app = builder.Build();
-
-// Dev only: Swagger UI et redirection HTTPS
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    app.UseHttpsRedirection(); // Redirection vers HTTPS uniquement en local
-}
 
 app.UseAuthorization();
 app.MapControllers();
